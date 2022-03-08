90786838f12cb84dbec62010f32d0b47a95463cb
sinta1919 <92878485+sinta1919@users.noreply.github.com>
2022-03-08 11:35:47 +0700

    Add files via upload

---
+++ latihan pv/latihan pv/MainForm.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: user
+ * Date: 07/03/2022
+ * Time: 22:49
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace latihan_pv
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void DoneClick(object sender, EventArgs e) 
+		{
+			MessageBox.Show ( "Nama :"+this.textBox1.Text +"\r"+"Nim :"
+			                 + this.textBox2.Text +"\r"+"Kom :" + this.textBox3.Text);
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+	MessageBox.Show ( "Nama :"+this.textBox1.Text +"\r"+"Nim :"
+			                 + this.textBox2.Text +"\r"+"Kom :" + this.textBox3.Text);
+		}
+	}
+}
